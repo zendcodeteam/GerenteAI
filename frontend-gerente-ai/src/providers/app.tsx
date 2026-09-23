b46dbfd6cfec5fbbe3aef9fd05dd14f59ffd9ead
Jhoan Camilo <acostadevice@gmail.com>
2026-09-23 11:42:39 -0500

    fix(frontend): use system theme by default

--- frontend-gerente-ai/src/providers/app.tsx
+++ frontend-gerente-ai/src/providers/app.tsx
@@ -8,7 +8,7 @@ type AppProviderProps = {
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <AuthProvider>
         {children}
       </AuthProvider>
